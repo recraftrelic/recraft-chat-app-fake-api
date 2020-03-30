d4b45709e76a2ce66594aed47cf90e3f8156f2e2
rocksinghajay <rocksinghajay@gmail.com>
2020-03-30 14:58:47 +0530

    added swagger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express'
 import { rootHandler, helloHandler } from './handlers';
+const swaggerDocument = require('../swagger.json')
 
 const app = express();
 const port = process.env.PORT || '8000';
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', rootHandler);
 app.get('/hello/:name', helloHandler);
