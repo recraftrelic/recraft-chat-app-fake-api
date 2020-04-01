6db1c44bc02ba5c18260b0e318b9c85ec5a41a83
rocksinghajay <rocksinghajay@gmail.com>
2020-04-01 13:25:54 +0530

    added login validation

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,18 +1,20 @@
 import jsonServer from 'json-server';
-const is = require("is_js");
-import loginValidations from "../validation/login";
-//const { validate } = require("micro-validator").default
+
 import dataBase from '../db.json';
 
+const is = require("is_js");
 const loginRouter = jsonServer.create();
 
 loginRouter.post("/", async (req, res) => {
-  //const validationErrors = validate(loginValidations, req.body)
   const { email, password } = req.body;
-  // if (!is.empty(validationErrors)) {
-  //   return res.status(400).json({ errors: validationErrors })
-  // }
-
+  if (!email) {
+    return res.status(400).json({ errors: "Email is required" })
+  } else if (!is.email(email)) {
+    return res.status(400).json({ errors: "Email is invalid" })
+  }
+  if (!password) {
+    return res.status(400).json({ errors: "Password is required" })
+  }
   try {
     let useExist = dataBase.users.find(user => user.email === email && user.password === password)
     if (useExist) {
